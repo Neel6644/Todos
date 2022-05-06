a8eb23b6423b6ed3113d985efc4cf50286d575cb
Neel Patel <neel6644@gmail.com>
2022-05-06 21:59:45 +0100

    change

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express=require ('express');
 const app=express();
 const cors=require('cors');
+const path = require('path');
+const PORT=process.env.PORT || 3000;
+
 //middleware 
 app.use(cors());
 app.use(express.json());
 
+if(process.env.NODE_ENV === 'producation'){
+    //server static content 
+    app.use(express.static('./client/build'));
+}
+
 app.use('/auth',require('./Route/login_register'));
 app.use('/dashboard',require('./Route/dashboard'));
 
-app.listen(3000,()=>{
-    console.log('server is running on 3000');
+//
+
+app.get('*',(req,res)=>{
+    res.sendFile(path.join(__dirname,'client/build/index.html'))
+});
+
+
+
+app.listen(PORT,()=>{
+    console.log(`server is running on ${PORT}`);
 })
